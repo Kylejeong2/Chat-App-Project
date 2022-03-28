256aff5218b774eaa59f7d56134464e1c47408a0
Kylejeong2 <Kylejeong21@gmail.com>
2022-03-28 13:42:47 -0700

    update

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ from helpers import apology, login_required
 
 #configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
