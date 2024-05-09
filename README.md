ae4cbee8bc765a91499a19348fb0b83e4bb3f57e
Kylejeong2 <Kylejeong21@gmail.com>
2024-05-09 11:52:06 -0700

    fixed issues project working

---
+++ README.md
@@ -0,0 +1,93 @@
+# Kcord
+ 
+#### Description: Kcord is an online chat app where there is a messaging channel, that people have to create accounts for via a login and register function. The app is mimicking the popular application "discord", but without many of the features. Using Flask, the application will require a login to use it's function. The application is powered by Flask and SocketIO, a library used with flask to create chat applications. The app.py takes care of all of the login functionality, and the login info is saved in a SQL database. The app.py also has the index which takes you to the chat page. The chat, login, and register pages are all derived from the layout.html file using Jinja as the template. I considered creating multiple chat channels in the server, but 1 centralized channel is sufficient for the purpose of the application. The application was designed for just the chat functionality. I also considered having the messages be anonymous messages and having the platform mimic the likes of the popular anonymous chat website known as Omegle, but I ultimately chose having the users be identified, as I see more functionality with the identified users. 
+
+<div id="top"></div>
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/Kylejeong2/Kcord">
+  </a>
+  <p>https://www.linkedin.com/in/kyle-jeong/</p>
+<h3 align="center">Kcord</h3>
+
+<div align="left">
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+See top of File ^
+
+<!-- 
+<img src="/images/screenshot.png" alt="ss of site">
+-->
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+### Built With
+
+* [Flask](https://flask.palletsprojects.com/en/2.0.x/)
+* [Python](https://www.python.org/)
+* [Javascript](https://www.javascript.com/)
+* [HTML](https://html.com/)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Bootstrap](https://getbootstrap.com)
+* [JINJA](https://jinja.palletsprojects.com/en/3.1.x/)
+* [JQuery](https://jquery.com/)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [x] Login Functionality
+- [x] Chat Functionality
+- [x] Full functioning html & css
+- [x] coloring on chat messages
+
+See the [open issues](https://github.com/Kylejeong2/Kcord/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- CONTACT -->
+## Contact
+
+Kyle Jeong - [@Kylejeong2](https://twitter.com/kylejeong21) - Kylejeong21@gmail.com
+
+Project Link: [https://github.com/Kylejeong2/Kcord](https://github.com/Kylejeong2/Kcord)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+ 
+  </div>
+
+ <!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/Kylejeong2/Kcord.svg?style=for-the-badge
+[contributors-url]: https://github.com/Kylejeong2/Kcord/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/Kylejeong2/Kcord.svg?style=for-the-badge
+[forks-url]: https://github.com/Kylejeong2/Kcord/network/members
+[stars-shield]: https://img.shields.io/github/stars/Kylejeong2/Kcord.svg?style=for-the-badge
+[stars-url]: https://github.com/Kylejeong2/Kcord/stargazers
+[issues-shield]: https://img.shields.io/github/issues/Kylejeong2/Kcord.svg?style=for-the-badge
+[issues-url]: https://github.com/Kylejeong2/Kcord/issues
+[license-shield]: https://img.shields.io/github/license/Kylejeong2/Kcord.svg?style=for-the-badge
+[license-url]: https://github.com/Kylejeong2/Kcord/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://www.linkedin.com/in/kyle-jeong/
+[product-screenshot]: images/screenshot.png
---
+++ app.py
@@ -0,0 +1,133 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import date
+from flask_socketio import SocketIO
+
+#import helper functions
+from helpers import apology, login_required
+
+#configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+#configure socket
+app.config['SECRET_KEY'] = 'ty2hfoupal2kddi[2ihf['
+socketio = SocketIO(app)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#configure database
+db = SQL("sqlite:///Kcord.db")
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"]= "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    if request.method == "POST":
+        return redirect("/chat")
+    else:
+        return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session.clear()
+
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        db_usernames = db.execute("SELECT username FROM users")
+
+        for db_username in db_usernames:
+            if request.form.get("username") == db_username["username"]:
+                return apology("username taken, please try again", 400)
+
+        if not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        if not request.form.get("confirmation"):
+            return apology("most confirm password", 400)
+
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("both passwords must match", 400)
+
+        password = request.form.get("password")
+        special = ["!",'"',"#","$","%","&","'","(",")","*", '+', ",","-", ".","/",":",";","<","=",">","?","@","[","]", "^", "_","`","{","|","}","~","]"]
+        s_count = 0
+
+        for i in special:
+            for word in password:
+                if i == word:
+                    s_count = s_count + 1
+
+        if not s_count >= 1:
+            return apology("Password needs a special character", 400)
+
+        db.execute("INSERT INTO users (username, hash) VALUES(?,?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
+
+        return redirect("/login")
+    else:
+        return render_template("register.html")
+
+@app.route("/chat")
+@login_required
+def chat():
+    usernames = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    username = ""
+    for user in usernames:
+        username = user["username"]
+
+    return render_template('chat.html', username=username)
+
+def message_received(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=message_received)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
