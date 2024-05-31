ff182fe3b1bf376ab0fad60240272a86009f463b
Kylejeong2 <Kylejeong21@gmail.com>
2024-05-30 21:44:20 -0700

    comments + cleaning up code

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import date
-from flask_socketio import SocketIO
+from cs50 import SQL #was for cs50x course, used package to access sqlite3
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session #holds the chat session
+from werkzeug.security import check_password_hash, generate_password_hash #for hashing passwords to put into database
+from flask_socketio import SocketIO #to save / facilitate the chat itself
 
 #import helper functions
 from helpers import apology, login_required
@@ -27,25 +25,18 @@ Session(app)
 #configure database
 db = SQL("sqlite:///Kcord.db")
 
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"]= "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"]) #initial index page to join the chat
 @login_required
 def index():
-    if request.method == "POST":
+    if request.method == "POST":  #clicking the join button
         return redirect("/chat")
     else:
         return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    session.clear()
-    if request.method == "POST":
+    session.clear() #clears prev logged in user (if exists)
+    if request.method == "POST": #when login form submitted
 
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -58,15 +49,15 @@ def login():
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["id"] #sets a userID (app always checking for userId)
 
-        return redirect("/")
+        return redirect("/") #redirects to the index 
     else:
         return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    session.clear()
+    session.clear() 
     return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -78,7 +69,7 @@ def register():
         if not request.form.get("username"):
             return apology("must provide username", 400)
 
-        db_usernames = db.execute("SELECT username FROM users")
+        db_usernames = db.execute("SELECT username FROM users") #get all usernames from DB to make sure its not taken
 
         for db_username in db_usernames:
             if request.form.get("username") == db_username["username"]:
@@ -97,7 +88,7 @@ def register():
         special = ["!",'"',"#","$","%","&","'","(",")","*", '+', ",","-", ".","/",":",";","<","=",">","?","@","[","]", "^", "_","`","{","|","}","~","]"]
         s_count = 0
 
-        for i in special:
+        for i in special: #counts number of special chars in password
             for word in password:
                 if i == word:
                     s_count = s_count + 1
@@ -114,20 +105,20 @@ def register():
 @app.route("/chat")
 @login_required
 def chat():
-    usernames = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    usernames = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"]) #gets all usernames from DB
     username = ""
     for user in usernames:
-        username = user["username"]
+        username = user["username"] #sets correct username for chat.html
 
     return render_template('chat.html', username=username)
 
-def message_received(methods=['GET', 'POST']):
+def message_received(methods=['GET', 'POST']): #used for testing
     print('message was received!!!')
 
-@socketio.on('my event')
+@socketio.on('my event') #custom code from SocketIO documentation
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=message_received)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True) #using socketIO to run the app
--- helpers.py
+++ helpers.py
@@ -1,21 +1,18 @@
-import os
-import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-def apology(message, code=400):
-    def escape(s):
+def apology(message, code=400): #if something is broken or not available redirects users to apology page
+    def escape(s): #fixes all of the chars that could potentially break the apology screen
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+    return render_template("apology.html", top=code, bottom=escape(message)), code #flask to render apology screen
 
-def login_required(f):
+def login_required(f): #checks if the user is currently logged in 
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect("/login")
+            return redirect("/login") #if not logged in then send to the login page
         return f(*args, **kwargs)
     return decorated_function
